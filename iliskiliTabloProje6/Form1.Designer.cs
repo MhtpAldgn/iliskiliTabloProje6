3cd4c2224ed3eb4c2b0622eaa0f0ed9293d17789
Asus <Asus@DESKTOP-B2AV4A5>
2023-03-04 20:15:25 +0300

    Proje dosyası ekle.

---
+++ iliskiliTabloProje6/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace iliskiliTabloProje6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pERSONELDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.üRÜNDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mÜŞTERİLERDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fIYATDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.proje6BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.İliskiliTabloProjeDataSet = new iliskiliTabloProje6.İliskiliTabloProjeDataSet();
+            this.proje6TableAdapter = new iliskiliTabloProje6.İliskiliTabloProjeDataSetTableAdapters.Proje6TableAdapter();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbMusteri = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbUrun = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbPersonel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proje6BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.İliskiliTabloProjeDataSet)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.pERSONELDataGridViewTextBoxColumn,
+            this.üRÜNDataGridViewTextBoxColumn,
+            this.mÜŞTERİLERDataGridViewTextBoxColumn,
+            this.fIYATDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.proje6BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(932, 232);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // pERSONELDataGridViewTextBoxColumn
+            // 
+            this.pERSONELDataGridViewTextBoxColumn.DataPropertyName = "PERSONEL";
+            this.pERSONELDataGridViewTextBoxColumn.HeaderText = "PERSONEL";
+            this.pERSONELDataGridViewTextBoxColumn.Name = "pERSONELDataGridViewTextBoxColumn";
+            // 
+            // üRÜNDataGridViewTextBoxColumn
+            // 
+            this.üRÜNDataGridViewTextBoxColumn.DataPropertyName = "ÜRÜN";
+            this.üRÜNDataGridViewTextBoxColumn.HeaderText = "ÜRÜN";
+            this.üRÜNDataGridViewTextBoxColumn.Name = "üRÜNDataGridViewTextBoxColumn";
+            // 
+            // mÜŞTERİLERDataGridViewTextBoxColumn
+            // 
+            this.mÜŞTERİLERDataGridViewTextBoxColumn.DataPropertyName = "MÜŞTERİLER";
+            this.mÜŞTERİLERDataGridViewTextBoxColumn.HeaderText = "MÜŞTERİLER";
+            this.mÜŞTERİLERDataGridViewTextBoxColumn.Name = "mÜŞTERİLERDataGridViewTextBoxColumn";
+            // 
+            // fIYATDataGridViewTextBoxColumn
+            // 
+            this.fIYATDataGridViewTextBoxColumn.DataPropertyName = "FIYAT";
+            this.fIYATDataGridViewTextBoxColumn.HeaderText = "FIYAT";
+            this.fIYATDataGridViewTextBoxColumn.Name = "fIYATDataGridViewTextBoxColumn";
+            // 
+            // proje6BindingSource
+            // 
+            this.proje6BindingSource.DataMember = "Proje6";
+            this.proje6BindingSource.DataSource = this.İliskiliTabloProjeDataSet;
+            // 
+            // İliskiliTabloProjeDataSet
+            // 
+            this.İliskiliTabloProjeDataSet.DataSetName = "İliskiliTabloProjeDataSet";
+            this.İliskiliTabloProjeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // proje6TableAdapter
+            // 
+            this.proje6TableAdapter.ClearBeforeFill = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.txtFiyat);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbMusteri);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cmbUrun);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbPersonel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(932, 131);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BİLGİ EKLEME";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(325, 83);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(271, 33);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(801, 33);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(101, 28);
+            this.txtFiyat.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(749, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fiyat:";
+            // 
+            // cmbMusteri
+            // 
+            this.cmbMusteri.FormattingEnabled = true;
+            this.cmbMusteri.Location = new System.Drawing.Point(560, 33);
+            this.cmbMusteri.Name = "cmbMusteri";
+            this.cmbMusteri.Size = new System.Drawing.Size(159, 29);
+            this.cmbMusteri.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(474, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Müşteriler:";
+            // 
+            // cmbUrun
+            // 
+            this.cmbUrun.FormattingEnabled = true;
+            this.cmbUrun.Location = new System.Drawing.Point(315, 33);
+            this.cmbUrun.Name = "cmbUrun";
+            this.cmbUrun.Size = new System.Drawing.Size(140, 29);
+            this.cmbUrun.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(239, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ürün Ad:";
+            // 
+            // cmbPersonel
+            // 
+            this.cmbPersonel.FormattingEnabled = true;
+            this.cmbPersonel.Location = new System.Drawing.Point(76, 33);
+            this.cmbPersonel.Name = "cmbPersonel";
+            this.cmbPersonel.Size = new System.Drawing.Size(140, 29);
+            this.cmbPersonel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Personel:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkMagenta;
+            this.ClientSize = new System.Drawing.Size(933, 380);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proje6BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.İliskiliTabloProjeDataSet)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private İliskiliTabloProjeDataSet İliskiliTabloProjeDataSet;
+        private System.Windows.Forms.BindingSource proje6BindingSource;
+        private İliskiliTabloProjeDataSetTableAdapters.Proje6TableAdapter proje6TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pERSONELDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn üRÜNDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mÜŞTERİLERDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fIYATDataGridViewTextBoxColumn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbMusteri;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbUrun;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbPersonel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ iliskiliTabloProje6/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace iliskiliTabloProje6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-B2AV4A5\\SQLDEVELOPER;Initial Catalog=İliskiliTabloProje;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // sqlden proedür getirme
+            //SqlDataAdapter da5 = new SqlDataAdapter("execute proje6", baglanti);
+            //DataTable dt5 = new DataTable();
+            //da5.Fill(dt5);
+            //dataGridView1.DataSource = dt5;
+
+            this.proje6TableAdapter.Fill(this.İliskiliTabloProjeDataSet.Proje6);
+
+            //combobox a ürünleri çekme
+            baglanti.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from tblurunler", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cmbUrun.ValueMember = "ID";
+            cmbUrun.DisplayMember = "AD";
+            cmbUrun.DataSource = dt;
+            baglanti.Close();
+
+            baglanti.Open();
+            SqlDataAdapter da2 = new SqlDataAdapter("select * from tblmusterıler", baglanti);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            cmbMusteri.ValueMember = "ID";
+            cmbMusteri.DisplayMember = "ADSOYAD";
+            cmbMusteri.DataSource = dt2;
+            baglanti.Close();
+
+            baglanti.Open();
+            SqlDataAdapter da3 = new SqlDataAdapter("select * from tblPERSONELLER", baglanti);
+            DataTable dt3 = new DataTable();
+            da3.Fill(dt3);
+            cmbPersonel.ValueMember = "ID";
+            cmbPersonel.DisplayMember = "AD";
+            cmbPersonel.DataSource = dt3;
+            baglanti.Close();
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand kmt = new SqlCommand("insert into tblhareketler (URUN,MUSTERI,PERSONEL,FIYAT) values(@E1,@E2,@E3,@E4)", baglanti);
+            kmt.Parameters.AddWithValue("@E1", cmbUrun.SelectedValue);
+            kmt.Parameters.AddWithValue("@E2", cmbMusteri.SelectedValue);
+            kmt.Parameters.AddWithValue("@E3", cmbPersonel.SelectedValue);
+            kmt.Parameters.AddWithValue("@E4", short.Parse(txtFiyat.Text));
+            kmt.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Bilgiler Eklendi");
+            this.proje6TableAdapter.Fill(this.İliskiliTabloProjeDataSet.Proje6);
+
+        }
+    }
+}
